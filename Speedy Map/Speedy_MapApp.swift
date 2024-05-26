0daad16beaad74bd74c15e5d3ad22d1cb7b5a4d5
Tom Bredemeier <tbredemeier@barrington220.org>
2024-05-26 13:42:08 -0500

    Initial Commit

---
+++ Speedy Map/Speedy_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speedy_MapApp.swift
+//  Speedy Map
+//
+//  Created by Tom Bredemeier on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Speedy_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
